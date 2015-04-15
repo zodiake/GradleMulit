3999ba32052d8be81de99cc00d14059279bf4f80
yagamai <zodiake.wang@ihotkeys.com>
2015-04-15 18:11:52 +0800

    fifth

--- repository/src/main/java/com/sj/repository/Application.java
+++ repository/src/main/java/com/sj/repository/Application.java
@@ -1,10 +1,17 @@
 package com.sj.repository;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.sj")
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(Application.class, args);
 	}
 
 }
