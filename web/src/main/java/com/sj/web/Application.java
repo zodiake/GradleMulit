e18cc64ce5423c35c5ffcf4ff40baa528b1223a9
zodiake <zodiake.wang@ihotkeys.com>
2015-04-15 23:04:28 +0800

    add

--- web/src/main/java/com/sj/web/Application.java
+++ web/src/main/java/com/sj/web/Application.java
@@ -7,13 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.sj"})
-@EnableJpaRepositories(basePackages = {"com.sj.repository.repository"})
+@Import(com.sj.repository.Application.class)
 public class Application {
 
 	public static void main(String[] args) {
