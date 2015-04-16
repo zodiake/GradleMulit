eefdd15d816997fb08a9620901b988903df7921d
yagamai <zodiake.wang@ihotkeys.com>
2015-04-16 14:05:13 +0800

    add all

--- model/src/main/java/com/sj/model/Application.java
+++ model/src/main/java/com/sj/model/Application.java
@@ -1,8 +1,14 @@
 package com.sj.model;
 
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EntityScan(basePackages = "com.sj.model.model")
 public class Application {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 	}
 
