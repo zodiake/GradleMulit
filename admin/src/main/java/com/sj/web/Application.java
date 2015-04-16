35426be369d3a6fc6399042527f38cbae58d36f2
yagamai <zodiake.wang@ihotkeys.com>
2015-04-16 15:20:47 +0800

    add admin module add profile

---
+++ admin/src/main/java/com/sj/web/Application.java
@@ -0,0 +1,54 @@
+package com.sj.web;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ConversionServiceFactoryBean;
+import org.springframework.core.convert.converter.Converter;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@Import(value = { com.sj.repository.Application.class,
+		com.sj.model.Application.class })
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	/*
+	 * @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) protected static class
+	 * SecurityConfig extends WebSecurityConfigurerAdapter {
+	 * 
+	 * @Resource(name = "authService") private UserDetailsService service;
+	 * 
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+	 * .antMatchers("/center/**").hasRole("USER").anyRequest()
+	 * .permitAll().and().formLogin().defaultSuccessUrl("/home")
+	 * .usernameParameter("name").passwordParameter("password")
+	 * .loginPage("/login").failureUrl("/login?error").permitAll(); }
+	 * 
+	 * @Override public void configure(AuthenticationManagerBuilder auth) throws
+	 * Exception {
+	 * auth.userDetailsService(service).passwordEncoder(passwordEncoder()); }
+	 * 
+	 * @Bean public ShaPasswordEncoder passwordEncoder() throws Exception {
+	 * return new ShaPasswordEncoder(256); } }
+	 */
+
+	@Bean
+	public ConversionServiceFactoryBean conversionService() {
+		ConversionServiceFactoryBean factoryBean = new ConversionServiceFactoryBean();
+		Set<Converter> converters = new HashSet<Converter>();
+		factoryBean.setConverters(converters);
+		return factoryBean;
+	}
+}
--- model/src/main/java/com/sj/model/model/Category.java
+++ model/src/main/java/com/sj/model/model/Category.java
@@ -1,17 +1,45 @@
 package com.sj.model.model;
 
+import java.util.Calendar;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.sj.model.type.ActivateEnum;
 
 @Entity
-@Table(name="category")
+@Table(name = "category")
 public class Category {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
+
 	private String name;
+
+	@OneToMany(mappedBy = "parent")
+	private Set<Category> categories;
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	@Enumerated(EnumType.STRING)
+	private ActivateEnum activate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar createdTime;
+	
+	private String createdBy;
+	
 }
