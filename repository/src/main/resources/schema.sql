ac3bc7c50657aa4e7acea2a9081ae2e0e6d5698b
464211646 <464211646@qq.com>
2015-07-28 11:18:49 +0800

    new database

--- repository/src/main/resources/schema.sql
+++ repository/src/main/resources/schema.sql
@@ -8,6 +8,7 @@ create table category(
 	updated_time timestamp,
 	updated_by varchar(20),
 	category_type char(2),
+	url varchar(50),
 	primary key(id),
 	foreign key (parent_id) references category(id)
 );
@@ -32,16 +33,17 @@ create table content(
 	content varchar(4000),
 	primary key (id)
 );
-
+-- 专题
 create table subject(
 	id bigint not null auto_increment,
 	name varchar(40) not null,
 	show_on_index smallint,
 	activate smallint,
-	category_id bigint,
 	created_time timestamp,
+	created_by varchar(64),
+	content_id bigint,
 	primary key(id),
-	foreign key (category_id) references category(id)
+	foreign key (content_id) references content(id)
 );
 
 create table site_user(
@@ -54,6 +56,9 @@ create table site_user(
 	site_authority varchar(20),
 	created_time timestamp,
 	authenticated_time timestamp,
+--	普通用户的真实姓名，企业用户的联系人
+	real_name varchar(20),			
+	sex smallint,
 	primary key(id)
 );
 
@@ -68,7 +73,7 @@ create TABLE provider(
 	business_license_url varchar(40),
 	tax_registration_url varchar(40),
 	structure_code_url varchar(40),
-	provider_name varchar(20),
+--	provider_name varchar(20),
 	position varchar(20),
 	provider_phone varchar(20),
 	fax varchar(20),
@@ -77,7 +82,7 @@ create TABLE provider(
 	website varchar(50),
 	province_id bigint,
 	city_id bigint,
-	provider_sex smallint,
+--	provider_sex smallint,
 	compony_type smallint,
 	business_type smallint,
 	scale smallint,
@@ -92,8 +97,8 @@ create TABLE provider(
 
 create table common_user(
 	id bigint not NULL auto_increment,
-	real_name varchar(20),
-	sex smallint,
+--	real_name varchar(20),
+--	sex smallint,
 	company varchar(64),
 	department varchar(64),
 	title smallint,
@@ -116,7 +121,7 @@ create table brand(
 	name varchar(50),
 	created_time timestamp,
 	activate smallint,
-	coverImg varchar(40),
+	cover_img varchar(40),
 	primary key(id),
 );
 
@@ -213,20 +218,26 @@ create table scroll_image(
 	primary key(id)
 );
 
-create table advertisement_content(
+create table information_content(
 	id bigint not null AUTO_INCREMENT,
 	content varchar(5000),
 	primary key(id)
 );
-
-create table advertisement(
+-- 资讯
+create table information(
 	id bigint not null AUTO_INCREMENT,
-	cover_img varchar(225),
+	title varchar(100),
 	content_id bigint,
 	created_time timestamp,
-	description varchar(200),
 	category_id bigint,
 	primary key(id),
-	foreign key(content_id) references advertisement_content(id),
+	foreign key(content_id) references information_content(id),
 	foreign key(category_id) references category(id)
+);
+--广告
+create table advertisement(
+	id bigint not null AUTO_INCREMENT,
+	category smallint,
+	cover_img varchar(50),
+	url varchar(100)
 );
