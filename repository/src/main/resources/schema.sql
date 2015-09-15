ac9c402b874e774c387f4a2eb0c69a7055160647
yagamai <zodiake.wang@ihotkeys.com>
2015-09-15 15:25:51 +0800

    try fix disconnect bug

--- repository/src/main/resources/schema.sql
+++ repository/src/main/resources/schema.sql
@@ -45,7 +45,7 @@ create table subject(
 	content_id bigint,
 	image varchar(69),
 	summary varchar(200),
-	category_id long,
+	category_id bigint,
 	primary key(id),
 	foreign key (content_id) references content(id),
 	foreign key (category_id) references category(id)
